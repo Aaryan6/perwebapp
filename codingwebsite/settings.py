fc66bc26325cb46d0edd4db30fa65472865e0cb4
Aaryan Patel <85422491+Aaryan6@users.noreply.github.com>
2021-07-16 07:10:44 +0530

    debug changed

--- codingwebsite/settings.py
+++ codingwebsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zb9%a_oh-%r^o&aqndjj00ndl3e38s6-r!_u&ceu5dta1l&6d7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','aaryanpatel.herokuapp.com']
 
